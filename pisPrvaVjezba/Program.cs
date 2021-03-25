4bc4cae329b1f30fa5df5aafe6979f8454c26782
Ivona <ivonaknaus@gmail.com>
2021-03-25 18:33:26 +0100

    Changes to be committed:

---
+++ pisPrvaVjezba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pisPrvaVjezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
+        }
+    }
+}
